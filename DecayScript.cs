b9a456339d95fa6318921e602a1a1c1ae355eef4
elJankowskiSchlawehner <40368687+elJankowskiSchlawehner@users.noreply.github.com>
2018-08-21 21:30:21 +0200

    Add files via upload

---
+++ DecayScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecayScript : MonoBehaviour
+{
+    private Transform _board;
+
+    public float m_tickRate = 0.3f;     // Tick-Rate
+    public float m_decayAt = 2.0f;      // Ab wann soll das Objekt verschwinden?
+    private float _decayTime;           // die "Schrumpfzeit", abhaengig von der Groesse des Objekts 
+    private bool b_isGrounded = false;  // noetig zur Ueberpruefung in Update
+
+    // Use this for initialization
+    void Start()
+    {
+        _decayTime = m_tickRate * transform.localScale.x;
+        _board = GameObject.Find("boardGameManager").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsGrounded() || transform.position.y < _board.position.y - 10f)
+        {
+            b_isGrounded = true;
+        }
+        if (b_isGrounded)
+        {
+            m_decayAt -= Time.deltaTime;
+        }
+        if (m_decayAt <= 0f)
+        {
+            if (transform.GetComponent<Rigidbody>() != null)
+            {
+                //transform.GetComponent<Rigidbody>().isKinematic = true;
+            }
+            transform.localScale = new Vector3(transform.localScale.x - (_decayTime * Time.deltaTime),
+                                                transform.localScale.y - (_decayTime * Time.deltaTime),
+                                                transform.localScale.z - (_decayTime * Time.deltaTime));
+        }
+
+        if (transform.localScale.x <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 2f);
+    }
+
+}
---
+++ Glass.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// steuert Spielstart
+
+public class Glass : MonoBehaviour {
+
+    public GameObject[] m_glassPrefabs;
+
+    private GameObject _player;
+
+	// Use this for initialization
+	void Start () {
+        _player = GameObject.Find("player");
+        Debug.Log("zum Spielen: Space druecken!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Spielstart bei Space???
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Glass instanzieren
+            Debug.Log("das Spiel beginnt ...");
+            GameObject glassBroken = Instantiate(m_glassPrefabs[Random.Range(0, m_glassPrefabs.Length)], gameObject.transform.position, gameObject.transform.rotation);
+            glassBroken.name = "glassBroken";
+
+            Rigidbody[] pieces = glassBroken.GetComponentsInChildren<Rigidbody>();
+
+            foreach (Rigidbody rb in pieces)
+            {
+                rb.gameObject.AddComponent<DecayScript>();
+                rb.AddExplosionForce(50.0f, transform.position, 5.0f, -5.0f);
+                Physics.IgnoreCollision(_player.gameObject.GetComponent<Collider>(), rb.gameObject.GetComponent<Collider>());
+            }
+            glassBroken.transform.DetachChildren();
+            Destroy(glassBroken);
+
+            // Spielereinstellungen vornehmen
+            _player.GetComponent<Rigidbody>().isKinematic = false;
+            _player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ
+                                                            | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+            // zerstoere das urspruengliche Glass
+            Destroy(gameObject);
+            
+        }
+    }
+}
